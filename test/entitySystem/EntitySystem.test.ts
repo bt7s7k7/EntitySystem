11bd50b443f9cc594aa79050015516f3ad95f378
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-11-24 12:07:20 +0100

    Added tests for EntitySystem emitting

--- test/entitySystem/EntitySystem.test.ts
+++ test/entitySystem/EntitySystem.test.ts
@@ -1,4 +1,6 @@
 import { expect } from "chai";
+import { Component } from "../../src/entitySystem/Component";
+import { Entity } from "../../src/entitySystem/Entity";
 import { EntitySystem } from "../../src/entitySystem/EntitySystem";
 import { EventListener } from "../../src/eventLib/EventListener";
 import { describeMember } from "../testUtil/describeMember";
@@ -34,6 +36,35 @@ describeMember(() => EntitySystem, () => {
             entitySystem.on(msg2Event).emit(58)
             msg1Tracker.check(1)
             msg2Tracker.check(1)
+        })
+
+        it("Should properly emit events", () => {
+            const entitySystem = new EntitySystem()
+
+            const eventTracker = tracker("event")
+            const updateEvent = new EntitySystem.EventDefinition<string>("update")
+
+            class CustomComponent extends Component {
+                protected update = this.system.on(updateEvent).add(this, (value) => {
+                    expect(value).to.equal("foo")
+                    eventTracker.trigger()
+                })
+            }
+
+            const entity = Entity.make()
+                .setSystem(entitySystem)
+                .addComponent(CustomComponent)
+                .build()
+
+            eventTracker.check(0)
+
+            entitySystem.on(updateEvent).emit("foo")
+
+            eventTracker.check(1)
+
+            entity.dispose()
+
+            eventTracker.check(1)
         })
     })
 })
